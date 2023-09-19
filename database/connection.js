17ae2f5247ea20c230fc34f60a0c37fb09bd1f9b
ViniciusMendonca12 <94076366+ViniciusMendonca12@users.noreply.github.com>
2023-09-19 11:10:54 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize('agritechacademy', 'root', 'familia100', {
+const connection = new Sequelize('*nome do banco*', 'root', '*senha*', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
